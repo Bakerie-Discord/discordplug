6b0e4145cd99173f7cfe2ab1d6bfe78d6c1073d9
actualdankcoder <zubi2048@gmail.com>
2021-12-15 23:22:43 +0530

    forgot css import

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavMenu from "../components/nav";
 import UFetchID from "../components/ufetcher";
+import "../styles/index.scss"
 export default function IDGen({id}) {
     console.log(id)
     return <div>
